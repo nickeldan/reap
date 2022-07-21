f9ad8309b415ed3376328cc394c0e5a6e4e7517a
Daniel Walker <nickeldan@gmail.com>
2022-07-20 22:57:08 -0400

    added comments to iterate_proc.h

--- include/reap/iterate_proc.h
+++ include/reap/iterate_proc.h
@@ -1,3 +1,12 @@
+/**
+ * @file iterate_proc.h
+ * @author Daniel Walker
+ * @brief Exposes process iteration.
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #ifndef REAP_ITERATE_PROC_H
 #define REAP_ITERATE_PROC_H
 
@@ -5,16 +14,42 @@
 
 #include "definitions.h"
 
+/**
+ * @brief Iterates over the running processes.
+ *
+ * @note User code should not access the iterator's fields.
+ */
 typedef struct reapProcIterator {
     DIR *dir;
 } reapProcIterator;
 
+/**
+ * @brief Initializes an iterator.
+ *
+ * @param iterator  A pointer to the iterator.
+ *
+ * @return          REAP_RET_OK if successful and an error code otherwise.
+ */
 int
 reapProcIteratorInit(reapProcIterator *iterator);
 
+/**
+ * @brief Closes an iterator.
+ *
+ * @param iterator  A pointer to the iterator.
+ */
 void
 reapProcIteratorClose(reapProcIterator *iterator);
 
+/**
+ * @brief Gets the next result.
+ *
+ * @param iterator      A pointer to the iterator.
+ * @param[out] info     The result to be populated.
+ *
+ * @return              REAP_RET_OK if a result was found, REAP_RET_DONE if the iteration is finished, and an
+ * error code otherwise.
+ */
 int
 reapProcIteratorNext(const reapProcIterator *iterator, reapProcInfo *info);
 
