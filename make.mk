bc0f9425b93866fa134fc89a8d8ac1de3b24929c
Daniel Walker <nickeldan@gmail.com>
2022-08-26 22:08:59 -0400

    make.mk now calls mkdir on any necessary directories

--- make.mk
+++ make.mk
@@ -17,7 +17,8 @@ BUILD_DEPS ?= $(if $(MAKECMDGOALS),$(subst clean,,$(MAKECMDGOALS)),yes)
 ifneq ($(BUILD_DEPS),)
 
 $(REAP_DEPS_FILE): $(REAP_SOURCE_FILES) $(REAP_HEADER_FILES)
-	rm -f $@
+	@mkdir -p $(@D)
+	@rm -f $@
 	for file in $(REAP_SOURCE_FILES); do \
 	    echo "$(REAP_OBJ_DIR)/`$(CC) $(REAP_INCLUDE_FLAGS) -MM $$file`" >> $@ && \
 	    echo '\t$$(CC) $$(CFLAGS) -fpic -ffunction-sections $(REAP_INCLUDE_FLAGS) -c $$< -o $$@' >> $@; \
@@ -27,9 +28,11 @@ include $(REAP_DEPS_FILE)
 endif
 
 $(REAP_SHARED_LIBRARY): $(REAP_OBJECT_FILES)
+	@mkdir -p $(@D)
 	$(CC) $(LDFLAGS) -shared -o $@ $^
 
 $(REAP_STATIC_LIBRARY): $(REAP_OBJECT_FILES)
+	@mkdir -p $(@D)
 	$(AR) rcs $@ $^
 
 reap_clean:
