96b7579157e69c128cc7501e727f255a9294fe8e
Daniel Walker <nickeldan@gmail.com>
2023-03-10 23:54:32 -0500

    testing now uses the shared library

--- tests/make.mk
+++ tests/make.mk
@@ -7,8 +7,8 @@ tests: $(TEST_BINARY)
 	@echo
 	@$<
 
-$(TEST_BINARY): $(TEST_SOURCE_FILES) $(REAP_STATIC_LIBRARY)
-	$(CC) $(CFLAGS) $(REAP_INCLUDE_FLAGS) $^ -lscrutiny -pthread -o $@
+$(TEST_BINARY): $(TEST_SOURCE_FILES) $(REAP_SHARED_LIBRARY)
+	$(CC) $(CFLAGS) $(REAP_INCLUDE_FLAGS) $(filter %.c,$^) -Wl,-rpath $(realpath $(REAP_LIB_DIR)) -L$(REAP_LIB_DIR) -lreap -lscrutiny -pthread -o $@
 
 test_clean:
 	@rm -f $(TEST_BINARY)
